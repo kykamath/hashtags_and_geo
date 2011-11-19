41a3c026d7ecf4501f1b8b85a461870f511ec41b
Krishna Kamath <krishna.kamath@gmail.com>
2011-11-19 09:46:13 -0600

    modifed analysis

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -3,9 +3,8 @@ Created on Nov 19, 2011
 
 @author: kykamath
 '''
-
 from library.mrjobwrapper import runMRJob
-from analysis.mr_analysis import MRAnalysis
+from mr_analysis import MRAnalysis
 
 tempInputFile = 'hdfs:///user/kykamath/geo/twitter/2_5'
 runMRJob(MRAnalysis, 'temp_file', [tempInputFile], jobconf={'mapred.reduce.tasks':90})
