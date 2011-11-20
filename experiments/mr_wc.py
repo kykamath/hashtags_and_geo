4c7f52c0a5422c587d64e0f07d1ca74f40e6bf09
Krishna Kamath <krishna.kamath@gmail.com>
2011-11-20 11:54:53 -0600

    modified analysis

--- experiments/mr_wc.py
+++ experiments/mr_wc.py
@@ -7,15 +7,14 @@ from library.mrjobwrapper import ModifiedMRJob
 
 class MRWC(ModifiedMRJob):
     DEFAULT_INPUT_PROTOCOL='raw_value'
-#    def configure_options(self):
-#        super(MRWC, self).configure_options()
-#        self.add_file_option( '--val', dest='val', default=' sdf ', help='provide initial clusters file')
-#        
-#    def load_options(self, args):
-#        """Parse stop_words option."""
-#        super(MRWC, self).load_options(args)
-#        self.val = self.options.val
-#        self.clusters = np.array(cjson.decode(data)['clusters'])
+    def configure_options(self):
+        super(MRWC, self).configure_options()
+        self.add_file_option( '--val', dest='val', default=' sdf ', help='provide initial clusters file')
+        
+    def load_options(self, args):
+        """Parse stop_words option."""
+        super(MRWC, self).load_options(args)
+        self.val = self.options.val
     def mapper(self, key, line):
         for w in line.split(): yield w, 1
     def reducer(self, key, values): yield key, [key, sum(list(values))]
