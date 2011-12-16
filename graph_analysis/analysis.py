53bb63ba146b20826fcac86a3514cc535f316d4b
Krishna Kamath <krishna.kamath@gmail.com>
2011-12-15 22:21:36 -0600

    modified analysis

--- graph_analysis/analysis.py
+++ graph_analysis/analysis.py
@@ -6,35 +6,57 @@ Created on Dec 15, 2011
 import sys, datetime
 from library.geo import getLocationFromLid, plotPointsOnWorldMap,\
     plotPointsOnUSMap, isWithinBoundingBox
+from library.classes import GeneralMethods
 sys.path.append('../')
 from library.mrjobwrapper import runMRJob
+from itertools import groupby
 from library.file_io import FileIO
 from graph_analysis.mr_modules import MRGraph
 from graph_analysis.settings import hdfsInputFolder, epochGraphsFile,\
     hashtagsFile, us_boundary
 import networkx as nx
+from operator import itemgetter
 from library.graphs import Networkx as my_nx
+from library.graphs import clusterUsingAffinityPropagation
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
+#    n_clusters_ = len(cluster_centers_indices)
+
+
 def plotLocationGraphOnMap(graph):
-#    for n in graph.node.keys()[:]:
-#        if not isWithinBoundingBox(getLocationFromLid(n.replace('_', ' ')), us_boundary): graph.remove_node(n)
-#    for node in graph.nodes():
     points = map(lambda  lid:getLocationFromLid(lid.replace('_', ' ')), graph.nodes())
     _, m =plotPointsOnUSMap(points, s=10, lw=0, c='m', returnBaseMapObject=True)
-    print graph.number_of_edges()
+    print graph.number_of_edges(), graph.number_of_nodes()
     totalEdgeWeight = max([d['w'] for _,_,d in graph.edges(data=True)])+0.0
     for u, v, data in graph.edges(data=True):
         u, v, w = getLocationFromLid(u.replace('_', ' ')), getLocationFromLid(v.replace('_', ' ')), data['w']
         m.drawgreatcircle(u[1],u[0],v[1],v[0],color=cm.Purples(w/totalEdgeWeight), alpha=0.5)
     plt.show()
-#    exit()
-def getGraphs(area, timeRange): return sorted([(d['ep'], my_nx.getGraphFromDict(d['graph']))for d in FileIO.iterateJsonFromFile(epochGraphsFile%(area, '%s_%s'%timeRange))])
 
+def plotLocationClustersOnMap(graph):
+    noOfClusters, clusters = clusterUsingAffinityPropagation(graph)
+    nodeToClusterIdMap = dict(clusters)
+    colorMap = dict([(i, GeneralMethods.getRandomColor()) for i in range(noOfClusters)])
+    clusters = [(c, list(l)) for c, l in groupby(sorted(clusters, key=itemgetter(1)), key=itemgetter(1))]
+    points, colors = zip(*map(lambda  l: (getLocationFromLid(l.replace('_', ' ')), colorMap[nodeToClusterIdMap[l]]), graph.nodes()))
+    _, m =plotPointsOnUSMap(points, s=30, lw=0, c=colors, returnBaseMapObject=True)
+    for u, v, data in graph.edges(data=True):
+        if nodeToClusterIdMap[u]==nodeToClusterIdMap[v]:
+            color, u, v, w = colorMap[nodeToClusterIdMap[u]], getLocationFromLid(u.replace('_', ' ')), getLocationFromLid(v.replace('_', ' ')), data['w']
+            m.drawgreatcircle(u[1],u[0],v[1],v[0],color=color, alpha=0.5)
+    plt.show()
+
+def getGraphs(area, timeRange): return sorted([(d['ep'], my_nx.getGraphFromDict(d['graph']))for d in FileIO.iterateJsonFromFile(epochGraphsFile%(area, '%s_%s'%timeRange))])
 def temp_analysis():
     for i, (ep, graph) in enumerate(getGraphs(area, timeRange)):
-        print i, plotLocationGraphOnMap(graph)
+        print datetime.datetime.fromtimestamp(ep)
+        plotLocationClustersOnMap(graph)
+#        clusters = clusterUsingAffinityPropagation(graph)[1]
+#        clusters = [(c, GeneralMethods.getRandomColor(), list(l)) for c, l in groupby(sorted(clusters, key=itemgetter(1)), key=itemgetter(1))]
+#        print datetime.datetime.fromtimestamp(ep) , graph.number_of_nodes(), sorted(clusters, key=itemgetter(1), reverse=True)
+#        exit()
+#        plotLocationGraphOnMap(graph)
 #    for e in sorted(epochs):
 #        print datetime.datetime.fromtimestamp(e)
 
@@ -44,8 +66,8 @@ def mr_task(timeRange, dataType, area):
     runMRJob(MRGraph, epochGraphsFile%(area, '%s_%s'%timeRange), getInputFiles(range(timeRange[0], timeRange[1]+1), dataType), jobconf={'mapred.reduce.tasks':160})   
 
 if __name__ == '__main__':
-    timeRange, dataType, area = (5,6), 'world', 'us'
-#    timeRange, dataType, area = (2,3), 'world', 'us'
+#    timeRange, dataType, area = (5,6), 'world', 'us'
+    timeRange, dataType, area = (5,6), 'world', 'world'
     
     mr_task(timeRange, dataType, area)
 #    temp_analysis()
