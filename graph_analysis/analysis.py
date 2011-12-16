a3f2b27400a4441a42d191344c49a55f5571b8dc
Krishna Kamath <krishna.kamath@gmail.com>
2011-12-16 10:45:52 -0600

    modified analysis

--- graph_analysis/analysis.py
+++ graph_analysis/analysis.py
@@ -8,7 +8,7 @@ import math
 sys.path.append('../')
 from library.geo import getLocationFromLid, plotPointsOnWorldMap,\
     plotPointsOnUSMap, isWithinBoundingBox
-from library.classes import GeneralMethods
+from library.classes import GeneralMethods, timeit
 from library.mrjobwrapper import runMRJob
 from itertools import groupby
 from library.file_io import FileIO
@@ -53,59 +53,65 @@ def combine(g1, g2):
     for u,v,data in g2.edges(data=True): updateNode(g1, u, g2.node[u]['w']), updateNode(g1, v, g2.node[v]['w']), updateEdge(g1, u, v, data['w'])
     return g1
 
-def linearCombineGraphs(graphMap, startingTime, intervalInSeconds):
-    if intervalInSeconds%TIME_UNIT_IN_SECONDS==0: numberOfGraphs = int(intervalInSeconds/TIME_UNIT_IN_SECONDS)
-    else: numberOfGraphs = int(intervalInSeconds/TIME_UNIT_IN_SECONDS)+1
-    graphId = GeneralMethods.approximateEpoch(GeneralMethods.getEpochFromDateTimeObject(startingTime), TIME_UNIT_IN_SECONDS)
-    graphIdsToCombine = map(lambda i: graphId-TIME_UNIT_IN_SECONDS*i, range(numberOfGraphs))
-    graphsToCombine = [graphMap[id] for id in graphIdsToCombine if id in graphMap]
-    return reduce(combine,graphsToCombine[1:],graphsToCombine[0])
+#def linearCombineGraphs(graphMap, startingTime, intervalInSeconds):
+#    if intervalInSeconds%TIME_UNIT_IN_SECONDS==0: numberOfGraphs = int(intervalInSeconds/TIME_UNIT_IN_SECONDS)
+#    else: numberOfGraphs = int(intervalInSeconds/TIME_UNIT_IN_SECONDS)+1
+#    graphId = GeneralMethods.approximateEpoch(GeneralMethods.getEpochFromDateTimeObject(startingTime), TIME_UNIT_IN_SECONDS)
+#    graphIdsToCombine = map(lambda i: graphId-TIME_UNIT_IN_SECONDS*i, range(numberOfGraphs))
+#    graphsToCombine = [graphMap[id] for id in graphIdsToCombine if id in graphMap]
+#    return reduce(combine,graphsToCombine[1:],graphsToCombine[0])
 
-def getLogarithmicGraphId(startingGraphId, graphId): return (graphId-startingGraphId)/TIME_UNIT_IN_SECONDS
-def logarithmicCombineGraphs(graphMap, startingGraphId, startingTime, intervalInSeconds):
-    print startingGraphId, startingTime, intervalInSeconds
+def getLogarithmicGraphId(startingGraphId, graphId): return ((int(graphId)-startingGraphId)/TIME_UNIT_IN_SECONDS)+1
+def getGraphId(startingGraphId, logarithmicGraphId): return startingGraphId+((logarithmicGraphId-1)*TIME_UNIT_IN_SECONDS)
+@timeit
+def combineLocationGraphs(graphMap, startingGraphId, startingTime, intervalInSeconds, linear=True, **kwargs):
     if intervalInSeconds%TIME_UNIT_IN_SECONDS==0: numberOfGraphs = int(intervalInSeconds/TIME_UNIT_IN_SECONDS)
     else: numberOfGraphs = int(intervalInSeconds/TIME_UNIT_IN_SECONDS)+1
     graphId = GeneralMethods.approximateEpoch(GeneralMethods.getEpochFromDateTimeObject(startingTime), TIME_UNIT_IN_SECONDS)
-    
-    currentValue = start = getLogarithmicGraphId(startingGraphId, graphId)
-    stop = start-numberOfGraphs
-    if stop<0: stop=0
+    currentLogarithmicId = getLogarithmicGraphId(startingGraphId, graphId)
+    currentCollectedGraphs = 0
     graphIdsToCombine = []
-    while currentValue>stop:
-        if currentValue%2==0: 
-            indices = map(lambda j: j*2, filter(lambda j: currentValue%(2**j)==0, range(1, int(math.log(currentValue+1,2))+1)))
-            for graphIdsToCombine in [map(lambda j: id-j*TIME_UNIT_IN_SECONDS, range(index)) for index in indices]:
-                graphsToCombine = [graphMap[j] for j in graphIdsToCombine if j in graphMap]
-                graphMap['%s_%s'%(id, len(graphIdsToCombine))] = reduce(combine,graphsToCombine[1:],graphsToCombine[0])
-        else: graphIdsToCombine.append(graphId-(start-currentValue)*TIME_UNIT_IN_SECONDS)
-    
-    print start, stop
+    while currentCollectedGraphs!=numberOfGraphs and currentLogarithmicId>0:
+        numberOfGraphsToCollect = 2**int(math.log(numberOfGraphs-currentCollectedGraphs,2))
+        if not linear and currentLogarithmicId%2==0: 
+            indices = [1]+map(lambda j: 2**j, filter(lambda j: currentLogarithmicId%(2**j)==0, range(1, int(math.log(currentLogarithmicId+1,2))+1)))
+            if max(indices)>numberOfGraphsToCollect and numberOfGraphsToCollect in indices: index = numberOfGraphsToCollect
+            else: index = max(indices)
+        else: index=1
+        logGraphId = '%s_%s'%(getGraphId(startingGraphId, currentLogarithmicId), index)
+        if logGraphId in graphMap: graphIdsToCombine.append(logGraphId)
+        currentLogarithmicId-=index
+        currentCollectedGraphs+=index
+    graphIdsToCombine = sorted(graphIdsToCombine, key=lambda id:int(id.split('_')[1]), reverse=True)
+    graphsToCombine = [graphMap[id] for id in graphIdsToCombine]
+    return reduce(combine,graphsToCombine[1:],graphsToCombine[0])
     
 def updateLogarithmicGraphs(graphMap):
     print 'Building logarithmic graphs... ',
     startingGraphId = sorted(graphMap.keys())[0]
-    for id in sorted(graphMap.keys()):
-        i = getLogarithmicGraphId(startingGraphId, id)+1
+    for graphId in sorted(graphMap.keys()):
+        i = getLogarithmicGraphId(startingGraphId, graphId)
         if i%2==0: 
-            indices = map(lambda j: j*2, filter(lambda j: i%(2**j)==0, range(1, int(math.log(i+1,2))+1)))
-            for graphIdsToCombine in [map(lambda j: id-j*TIME_UNIT_IN_SECONDS, range(index)) for index in indices]:
+            indices = map(lambda j: 2**j, filter(lambda j: i%(2**j)==0, range(1, int(math.log(i+1,2))+1)))
+            for graphIdsToCombine in [map(lambda j: graphId-j*TIME_UNIT_IN_SECONDS, range(index)) for index in indices]:
                 graphsToCombine = [graphMap[j] for j in graphIdsToCombine if j in graphMap]
-                graphMap['%s_%s'%(id, len(graphIdsToCombine))] = reduce(combine,graphsToCombine[1:],graphsToCombine[0])
+                graphMap['%s_%s'%(graphId, len(graphIdsToCombine))] = reduce(combine,graphsToCombine[1:],graphsToCombine[0])
+        graphMap['%s_%s'%(graphId, 1)] = graphMap[graphId]; del graphMap[graphId]
     print 'Completed!!'
     return startingGraphId
     
 def getGraphs(area, timeRange): return sorted([(d['ep'], my_nx.getGraphFromDict(d['graph']))for d in FileIO.iterateJsonFromFile(epochGraphsFile%(area, '%s_%s'%timeRange))])
 def temp_analysis():
     graphMap = dict(getGraphs(area, timeRange))
-    print len(graphMap)
-#    startingGraphId = updateLogarithmicGraphs(graphMap)
-    startingGraphId = sorted(graphMap)[0]
-    print len(graphMap)
-    startingTime, intervalInSeconds = datetime.datetime(2011,5,5,6,7,30), 24*TIME_UNIT_IN_SECONDS
-#    graph = linearCombineGraphs(graphMap, startingTime, intervalInSeconds)
-    graph = logarithmicCombineGraphs(graphMap, startingGraphId, startingTime, intervalInSeconds)
-
+#    print len(graphMap)
+    startingGraphId = updateLogarithmicGraphs(graphMap)
+#    startingGraphId = sorted(graphMap)[0]
+#    print len(graphMap)
+    startingTime, intervalInSeconds = datetime.datetime(2011,5,6,7,7,30), 60*TIME_UNIT_IN_SECONDS
+    graph, runningTime = combineLocationGraphs(graphMap, startingGraphId, startingTime, intervalInSeconds, linear=True, returnTimeDifferenceOnly=True)
+    print graph.number_of_nodes(), runningTime
+    graph, runningTime = combineLocationGraphs(graphMap, startingGraphId, startingTime, intervalInSeconds, linear=False, returnTimeDifferenceOnly=True)
+    print graph.number_of_nodes(), runningTime
 #    print clusterUsingAffinityPropagation(graph)
 
 #    for i, (ep, graph) in enumerate(getGraphs(area, timeRange)):
@@ -120,8 +126,8 @@ def mr_task(timeRange, dataType, area):
     runMRJob(MRGraph, epochGraphsFile%(area, '%s_%s'%timeRange), getInputFiles(range(timeRange[0], timeRange[1]+1), dataType), jobconf={'mapred.reduce.tasks':160})   
 
 if __name__ == '__main__':
-    timeRange, dataType, area = (5,6), 'world', 'us'
-#    timeRange, dataType, area = (5,6), 'world', 'world'
+#    timeRange, dataType, area = (5,6), 'world', 'us'
+    timeRange, dataType, area = (5,11), 'world', 'world'
     
-#    mr_task(timeRange, dataType, area)
-    temp_analysis()
+    mr_task(timeRange, dataType, area)
+#    temp_analysis()
