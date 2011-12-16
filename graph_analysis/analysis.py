d92c3d7aa63f1354e808c45ba9d1a91bea6af0b4
Krishna Kamath <krishna.kamath@gmail.com>
2011-12-15 19:25:29 -0600

    modified analysis

--- graph_analysis/analysis.py
+++ graph_analysis/analysis.py
@@ -47,5 +47,5 @@ if __name__ == '__main__':
     timeRange, dataType, area = (5,6), 'world', 'us'
 #    timeRange, dataType, area = (2,3), 'world', 'us'
     
-    mr_task(timeRange, dataType, area)
-#    temp_analysis()
+#    mr_task(timeRange, dataType, area)
+    temp_analysis()
