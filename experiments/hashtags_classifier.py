4dca6028214d57414d96ac983499a1cbc72078b6
Krishna Kamath <krishna.kamath@gmail.com>
2012-01-06 08:12:53 -0600

    modified analysis

--- experiments/hashtags_classifier.py
+++ experiments/hashtags_classifier.py
@@ -6,21 +6,13 @@ Created on Dec 11, 2011
 import sys
 sys.path.append('../')
 from library.file_io import FileIO
-from settings import hashtagsWithoutEndingWindowFile, hashtagsClassifiersFolder, hashtagsFile,\
+from settings import hashtagsClassifiersFolder, hashtagsFile,\
     hashtagsAnalysisFolder
-from experiments.mr_area_analysis import getOccuranesInHighestActiveRegion,\
-    getOccurranceDistributionInEpochs, HashtagsClassifier, getRadius
-from operator import itemgetter
-import math
-import numpy as np
-from sklearn.cross_validation import KFold
-from sklearn.svm import SVC, LinearSVC
+from sklearn.svm import SVC
 from experiments.models import Hashtag
 from sklearn.externals import joblib
 from library.classes import GeneralMethods
 
-
-timeRange, folderType = (2,11), 'world'
 
 class Classifier:
     FEATURES_RADIUS = 'radius'
@@ -44,9 +36,12 @@ class Classifier:
     def load(self): 
         if self.clf==None: self.clf = joblib.load(self.classfierFile)
         return self.clf
+    def predict(self, document):
+        if self.clf==None: self.clf = joblib.load(self.classfierFile)
+        return self.clf.predict(document)
     @staticmethod
     def testClassifierPerformance():
-        GeneralMethods.runCommand('rm -rf %s*'%Classifier.classifiersPerformanceFile)
+        GeneralMethods.runCommand('rm -rf %s'%Classifier.classifiersPerformanceFile)
         for feature in [Classifier.FEATURES_RADIUS, Classifier.FEATURES_OCCURANCES_RADIUS]:
             for numberOfTimeUnits in range(1,25):
                 documents = []
@@ -60,43 +55,33 @@ class Classifier:
                 FileIO.writeToFileAsJson({'features': classifier.features, 'numberOfTimeUnits': numberOfTimeUnits, 'score': classifier.score(testDocuments)}, Classifier.classifiersPerformanceFile)
     @staticmethod
     def buildClassifier():
-        for numberOfTimeUnits in range(1,25):
-            documents = []
-            classifier = Classifier(numberOfTimeUnits, features=Classifier.FEATURES_RADIUS)
-            for h in FileIO.iterateJsonFromFile(hashtagsFile%('training_world','%s_%s'%(2,11))):
-                ov = Hashtag(h, dataStructuresToBuildClassifier=True)
-                if ov.isValidObject() and ov.classifiable: 
-                    if classifier.features == Classifier.FEATURES_RADIUS: documents.append(ov.getVector(numberOfTimeUnits, radiusOnly=True))
-                    else: documents.append(ov.getVector(numberOfTimeUnits, radiusOnly=False))
-            trainDocuments = documents[:int(len(documents)*0.80)]
-            classifier.build(trainDocuments)
-#        print classifier.score(testDocuments)
-#X, y = zip(*documents)
-#X = np.array(X)
-#y = np.array(y)
-#kf = KFold(n=len(documents), k=3)
-#for i, (train_index, test_index) in enumerate(kf):
-#    X_train, X_test = X[train_index], X[test_index]
-#    y_train, y_test = y[train_index], y[test_index]
-#    clf = LinearSVC()
-#    clf.fit(X_train, y_train)
-#    print clf.score(X_test, y_test)
+        for feature in [Classifier.FEATURES_OCCURANCES_RADIUS]:
+            for numberOfTimeUnits in range(1,25):
+                documents = []
+                classifier = Classifier(numberOfTimeUnits, features=feature)
+                for h in FileIO.iterateJsonFromFile(hashtagsFile%('training_world','%s_%s'%(2,11))):
+                    ov = Hashtag(h, dataStructuresToBuildClassifier=True)
+                    if ov.isValidObject() and ov.classifiable: 
+                        if classifier.features == Classifier.FEATURES_RADIUS: documents.append(ov.getVector(numberOfTimeUnits, radiusOnly=True))
+                        else: documents.append(ov.getVector(numberOfTimeUnits, radiusOnly=False))
+                trainDocuments = documents[:int(len(documents)*0.80)]
+                classifier.build(trainDocuments)
 
-#trainDocuments = documents[:int(len(documents)*0.80)]
-#testDocuments = documents[:int(len(documents)*0.20)]
+documents = []
+numberOfTimeUnits = 1
+classifier = Classifier(numberOfTimeUnits, features=Classifier.FEATURES_RADIUS)
+for h in FileIO.iterateJsonFromFile(hashtagsFile%('training_world','%s_%s'%(2,11))):
+    ov = Hashtag(h, dataStructuresToBuildClassifier=True)
+    if ov.isValidObject() and ov.classifiable: 
+        if classifier.features == Classifier.FEATURES_RADIUS: documents.append(ov.getVector(numberOfTimeUnits, radiusOnly=True))
+        else: documents.append(ov.getVector(numberOfTimeUnits, radiusOnly=False))
+testDocuments = documents[-int(len(documents)*0.20):]
 
-#trainX, trainy = zip(*trainDocuments)
-#testX, testy = zip(*testDocuments)
-##clf = SVC(probability=True)
-##clf.fit(trainX, trainy)
-#FileIO.createDirectoryForFile('classifiers/abc.pkl')
-##joblib.dump(clf, 'classifiers/abc.pkl')
-#clf = joblib.load('classifiers/abc.pkl')
-#print clf.score(testX, testy)
+for t in testDocuments:
+    if classifier.predict(t[0])==1:
+        print t, classifier.predict(t[0])
 
-#0.937282229965
-
-
+#timeRange, folderType = (2,11), 'world'
 #Classifier(5).build(trainDocuments)
 #print Classifier(5).score(testDocuments)
-Classifier.testClassifierPerformance()
+#Classifier.buildClassifier()
