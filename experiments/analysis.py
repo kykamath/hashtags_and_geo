d0dfaaa524cf28328df7c09150ad484a1da04452
Krishna Kamath <krishna.kamath@gmail.com>
2012-01-08 13:26:22 -0600

    modified models

--- experiments/analysis.py
+++ experiments/analysis.py
@@ -51,43 +51,43 @@ from collections import defaultdict
 def getModifiedLatticeLid(point, accuracy=0.0075):
     return '%0.1f_%0.1f'%(int(point[0]/accuracy)*accuracy, int(point[1]/accuracy)*accuracy)
 
-def plotHashtagDistributionInTime():
-    for h in FileIO.iterateJsonFromFile(hashtagsDistributionInTimeFile):
-        if h['t']>300:
-            distribution = dict(h['d'])
-#            print unicode(h['h']).encode('utf-8'), h['t']
-            ax = plt.subplot(111)
-            dataX = sorted(distribution)
-            plt.plot_date([datetime.datetime.fromtimestamp(e) for e in dataX], [distribution[x] for x in dataX])
-            plt.title('%s (%s)'%(h['h'], h['t'])    )
-            plt.setp(ax.get_xticklabels(), rotation=30, fontsize=10)
-            plt.xlim(xmin=datetime.datetime(2011, 2, 1), xmax=datetime.datetime(2011, 11, 30))
-            plt.show()
-            
-class HashtagObject:
-    def __init__(self, hashtagDict): self.dict = hashtagDict
-    def getLocalLattice(self): return self.dict['liAtVaryingK'][0][1]
-    @staticmethod
-    def iterateHashtagObjects(timeRange, file=hashtagsAnalayzeLocalityIndexAtKFile, hastagsList = None):
-        for h in FileIO.iterateJsonFromFile(file%(outputFolder, '%s_%s'%timeRange)): 
-            if hastagsList==None or h['h'] in hastagsList: yield HashtagObject(h)
+#def plotHashtagDistributionInTime():
+#    for h in FileIO.iterateJsonFromFile(hashtagsDistributionInTimeFile):
+#        if h['t']>300:
+#            distribution = dict(h['d'])
+##            print unicode(h['h']).encode('utf-8'), h['t']
+#            ax = plt.subplot(111)
+#            dataX = sorted(distribution)
+#            plt.plot_date([datetime.datetime.fromtimestamp(e) for e in dataX], [distribution[x] for x in dataX])
+#            plt.title('%s (%s)'%(h['h'], h['t'])    )
+#            plt.setp(ax.get_xticklabels(), rotation=30, fontsize=10)
+#            plt.xlim(xmin=datetime.datetime(2011, 2, 1), xmax=datetime.datetime(2011, 11, 30))
+#            plt.show()
+#            
+#class HashtagObject:
+#    def __init__(self, hashtagDict): self.dict = hashtagDict
+#    def getLocalLattice(self): return self.dict['liAtVaryingK'][0][1]
+#    @staticmethod
+#    def iterateHashtagObjects(timeRange, file=hashtagsAnalayzeLocalityIndexAtKFile, hastagsList = None):
+#        for h in FileIO.iterateJsonFromFile(file%(outputFolder, '%s_%s'%timeRange)): 
+#            if hastagsList==None or h['h'] in hastagsList: yield HashtagObject(h)
 
-class HashtagClass:
-    hashtagsCSV = '../data/hashtags.csv'
-    @staticmethod
-    def createCSV(timeRange):
-        i=1
-        for h in FileIO.iterateJsonFromFile(hashtagsWithoutEndingWindowFile%(outputFolder, '%s_%s'%timeRange)):
-            FileIO.writeToFile(h['h']+', ', HashtagClass.hashtagsCSV)
-    @staticmethod
-    def iterateHashtagsWithClass():
-        for line in FileIO.iterateLinesFromFile(HashtagClass.hashtagsCSV): 
-            h, cls = line.strip().split(', ')   
-            yield h.strip(), cls.strip()
-    @staticmethod
-    def getHashtagClasses():
-        hashtags = sorted(HashtagClass.iterateHashtagsWithClass(), key=itemgetter(1))
-        return dict((cls, list(h[0] for h in hashtags)) for cls, hashtags in groupby(hashtags, key=itemgetter(1)))
+#class HashtagClass:
+#    hashtagsCSV = '../data/hashtags.csv'
+#    @staticmethod
+#    def createCSV(timeRange):
+#        i=1
+#        for h in FileIO.iterateJsonFromFile(hashtagsWithoutEndingWindowFile%(outputFolder, '%s_%s'%timeRange)):
+#            FileIO.writeToFile(h['h']+', ', HashtagClass.hashtagsCSV)
+#    @staticmethod
+#    def iterateHashtagsWithClass():
+#        for line in FileIO.iterateLinesFromFile(HashtagClass.hashtagsCSV): 
+#            h, cls = line.strip().split(', ')   
+#            yield h.strip(), cls.strip()
+#    @staticmethod
+#    def getHashtagClasses():
+#        hashtags = sorted(HashtagClass.iterateHashtagsWithClass(), key=itemgetter(1))
+#        return dict((cls, list(h[0] for h in hashtags)) for cls, hashtags in groupby(hashtags, key=itemgetter(1)))
     
 class Parameters:
     @staticmethod
