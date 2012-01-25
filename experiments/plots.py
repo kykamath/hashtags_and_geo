a3cbc0f34ee7e21eb1bd7ff888b23ad4bd82fa0d
Krishna Kamath <krishna.kamath@gmail.com>
2012-01-24 18:45:25 -0600

    modified plots

--- experiments/plots.py
+++ experiments/plots.py
@@ -20,6 +20,10 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from collections import defaultdict
 import numpy as np
 
+RIO_DE_JANEIRO = '-22.7650_-43.0650'
+NEW_YORK = '40.6000_-73.8050'
+SAO_PAULO = '-23.4900_-46.5450'
+
 class PlotGraphsOnMap:
     @staticmethod
     def plotGraphsForHashtag(hashtag):
@@ -38,7 +42,7 @@ class PlotGraphsOnMap:
                         occurancesGroupedByLattice = [(getLocationFromLid(lid.replace('_', ' ')), 'm') for lid, occs in groupby(sorted([(getLatticeLid(l, LATTICE_ACCURACY), t) for l, t in totalOccurances], key=itemgetter(0)), key=itemgetter(0))]
                         occurancesGroupedByLattice = sorted(occurancesGroupedByLattice, key=itemgetter(1))
                         points, colors = zip(*occurancesGroupedByLattice)
-                        plotPointsOnWorldMap(points, blueMarble=True, bkcolor='#CFCFCF', c=colors, lw = 0)
+                        plotPointsOnWorldMap(points, blueMarble=False, bkcolor='#CFCFCF', c=colors, lw = 0)
                         plt.show()
     #                    plt.savefig(fileName)
                         plt.clf()
@@ -68,10 +72,8 @@ class Locality:
     @staticmethod
     def _getDistances():
         distances = {}
-#        distances = {'similarity': defaultdict(dict), 'temporalDistance': defaultdict(dict), 'geoDistance': defaultdict(dict)}
         for latticeObject in FileIO.iterateJsonFromFile(hashtagsLatticeGraphFile%('training_world','%s_%s'%(2,11))):
             latticeHashtagsSet = set(latticeObject['hashtags'])
-#            distances['hashtagObservingProbability'][latticeObject['id']] = latticeHashtagsSet
             for neighborLattice, neighborHashtags in latticeObject['links'].iteritems():
                 key = '_'.join(sorted([latticeObject['id'], neighborLattice]))
                 if key not in distances:
@@ -81,9 +83,6 @@ class Locality:
                     distances[key]['similarity']=len(latticeHashtagsSet.intersection(neighborHashtagsSet))/float(len(latticeHashtagsSet.union(neighborHashtagsSet)))
                     distances[key]['temporalDistance']=np.mean([abs(latticeObject['hashtags'][k][0]-neighborHashtags[k][0]) for k in neighborHashtags if k in latticeObject['hashtags']])
                     distances[key]['geoDistance']=getHaversineDistanceForLids(latticeObject['id'].replace('_', ' '), neighborLattice.replace('_', ' '))
-#                    if distances[key]['similarity']==1: 
-#                        print 'x'
-#            distances['similarity'][latticeObject['id']][latticeObject['id']]=1.0
         return distances
     @staticmethod
     def plotTemporalLocality():
@@ -96,7 +95,7 @@ class Locality:
             print k, len(dataToPlot[k]), len(filter(lambda i:i<upperRange, dataToPlot[k]))
             plt.scatter(k, np.mean(filter(lambda i:i<upperRange, dataToPlot[k]))/(60.*60.), c='r', lw = 0)
 #        plt.show()
-        plt.title('Temporal distance between lattices'), plt.xlabel('Jakkard similarity'), plt.ylabel('Time difference (hours)')
+        plt.title('Temporal distance between lattices'), plt.xlabel('Jaccard similarity'), plt.ylabel('Mean time difference (hours)')
         plt.savefig('../images/temporalDistance.png')
     @staticmethod
     def plotSpatialLocality():
@@ -109,14 +108,69 @@ class Locality:
             print k, len(dataToPlot[k]), len(filter(lambda i:i<upperRange, dataToPlot[k]))
             plt.scatter(k, np.mean(filter(lambda i:i<upperRange, dataToPlot[k])), c='r', lw = 0)
 #        plt.show()
-        plt.title('Spatial locality between lattices'), plt.xlabel('Jakkard similarity'), plt.ylabel('Mean distance (miles)')
+        plt.title('Haversine distance between lattices'), plt.xlabel('Jaccard similarity'), plt.ylabel('Mean haversine distance (miles)')
         plt.savefig('../images/geoDistance.png')
+    @staticmethod
+    def temporalLocalitySimilarityExample(lattice=NEW_YORK):
+        distances = defaultdict(dict)
+        for latticeObject in FileIO.iterateJsonFromFile(hashtagsLatticeGraphFile%('training_world','%s_%s'%(2,11))):
+            if latticeObject['id']==lattice:
+                latticeHashtagsSet = set(latticeObject['hashtags'])
+                for neighborLattice, neighborHashtags in latticeObject['links'].iteritems():
+                    distances[neighborLattice] = {}
+                    neighborHashtags = filterOutNeighborHashtagsOutside1_5IQROfTemporalDistance(latticeObject['hashtags'], neighborHashtags, findLag=False)
+                    neighborHashtagsSet = set(neighborHashtags)
+                    distances[neighborLattice]['similarity']=len(latticeHashtagsSet.intersection(neighborHashtagsSet))/float(len(latticeHashtagsSet.union(neighborHashtagsSet)))
+                    distances[neighborLattice]['temporalDistance']=np.mean([abs(latticeObject['hashtags'][k][0]-neighborHashtags[k][0]) for k in neighborHashtags if k in latticeObject['hashtags']])/(60.*60.)
+                    distances[neighborLattice]['geoDistance']=getHaversineDistanceForLids(latticeObject['id'].replace('_', ' '), neighborLattice.replace('_', ' '))
+                break
+        dataPoints = []
+        ax = plt.subplot(111)
+        for k, data in distances.iteritems(): dataPoints.append((getLocationFromLid(k.replace('_', ' ')), data['similarity']))
+        points, colors = zip(*sorted(dataPoints, key=itemgetter(1)))
+        sc = plotPointsOnWorldMap(points, blueMarble=False, bkcolor='#CFCFCF', cmap='RdPu', c=colors, lw = 0, alpha=1.0)
+        plotPointsOnWorldMap([getLocationFromLid(lattice.replace('_', ' '))], blueMarble=False, bkcolor='#CFCFCF', c='#64FF1C', lw = 0)
+        divider = make_axes_locatable(ax)
+        plt.title('Jaccard similarity with New York')
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(sc, cax=cax)
+#        plt.show()
+        plt.savefig('../images/similarityExample.png')
+    @staticmethod
+    def temporalLocalityTemporalDistanceExample(lattice=NEW_YORK):
+        distances = defaultdict(dict)
+        for latticeObject in FileIO.iterateJsonFromFile(hashtagsLatticeGraphFile%('training_world','%s_%s'%(2,11))):
+            if latticeObject['id']==lattice:
+                latticeHashtagsSet = set(latticeObject['hashtags'])
+                for neighborLattice, neighborHashtags in latticeObject['links'].iteritems():
+                    distances[neighborLattice] = {}
+                    neighborHashtags = filterOutNeighborHashtagsOutside1_5IQROfTemporalDistance(latticeObject['hashtags'], neighborHashtags, findLag=False)
+                    neighborHashtagsSet = set(neighborHashtags)
+                    distances[neighborLattice]['similarity']=len(latticeHashtagsSet.intersection(neighborHashtagsSet))/float(len(latticeHashtagsSet.union(neighborHashtagsSet)))
+                    distances[neighborLattice]['temporalDistance']=np.mean([abs(latticeObject['hashtags'][k][0]-neighborHashtags[k][0]) for k in neighborHashtags if k in latticeObject['hashtags']])/(60.*60.)
+                    distances[neighborLattice]['geoDistance']=getHaversineDistanceForLids(latticeObject['id'].replace('_', ' '), neighborLattice.replace('_', ' '))
+                break
+        dataPoints = []
+        ax = plt.subplot(111)
+        for k, data in distances.iteritems(): dataPoints.append((getLocationFromLid(k.replace('_', ' ')), data['temporalDistance']))
+        points, colors = zip(*sorted(dataPoints, key=itemgetter(1)))
+        sc = plotPointsOnWorldMap(points, blueMarble=False, bkcolor='#CFCFCF', cmap='RdPu', c=colors, lw = 0, alpha=1.0)
+        plotPointsOnWorldMap([getLocationFromLid(lattice.replace('_', ' '))], blueMarble=False, bkcolor='#CFCFCF', c='#64FF1C', lw = 0)
+        divider = make_axes_locatable(ax)
+        plt.title('Average time difference from New York')
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(sc, cax=cax)
+#        plt.show()
+        plt.savefig('../images/temporalDistanceExample.png')
     @staticmethod
     def run():
         Locality.plotTemporalLocality()
 #        Locality.plotSpatialLocality()
+#        Locality.temporalLocalitySimilarityExample()
+#        Locality.temporalLocalityTemporalDistanceExample()
         
 if __name__ == '__main__':
 #    PlotGraphsOnMap.run()
-    Locality.run()    
+    Locality.run()
+#    print getLatticeLid([-23.549569,-46.639173],  0.145)
     
