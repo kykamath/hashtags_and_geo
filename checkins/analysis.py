a7956d1a752724784ee6b6e18c5fd8e6906719cb
Krishna Kamath <krishna.kamath@gmail.com>
2012-02-01 22:43:28 -0600

    added checkins

---
+++ checkins/analysis.py
@@ -0,0 +1,22 @@
+'''
+Created on Feb 1, 2012
+
+@author: kykamath
+'''
+import sys
+sys.path.append('../')
+from library.file_io import FileIO
+from datetime import datetime
+from checkins.settings import checkinsJSONFile
+def parseData(line):
+    data = line.strip().split('\t')
+    if len(data)!=7: data.append(None) 
+    if len(data)==7: return {'id':int(data[1]), 'u': int(data[0]), 'l': [float(data[2]), float(data[3])], 't': datetime.strptime(data[4], '%Y-%m-%d %H:%M:%S'), 'x': data[5], 'lid': data[6]}
+
+def writeCheckinsToJSONFormat():
+    for i, data in enumerate(FileIO.iterateLinesFromFile('/mnt/chevron/kykamath/data/geo/checkin_data.txt')):
+#    for i, data in enumerate(FileIO.iterateLinesFromFile('../data/checkin_data.txt')):
+        print i
+        FileIO.writeToFileAsJson(data, checkinsJSONFile)
+        
+writeCheckinsToJSONFormat()
---
+++ checkins/settings.py
@@ -0,0 +1,7 @@
+'''
+Created on Feb 1, 2012
+
+@author: kykamath
+'''
+checkinsFolder = '/mnt/chevron/kykamath/data/geo/checkins/'
+checkinsJSONFile = checkinsFolder+'checkins.json'
