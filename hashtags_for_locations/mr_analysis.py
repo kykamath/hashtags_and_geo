bb498224d49517246abf9adbbd8e93a899f70103
Krishna Kamath <krishna.kamath@gmail.com>
2012-02-22 11:54:59 -0600

    modified analysis

--- hashtags_for_locations/mr_analysis.py
+++ hashtags_for_locations/mr_analysis.py
@@ -24,7 +24,7 @@ LOCATION_ACCURACY = 0.145
 #START_TIME, END_TIME, WINDOW_OUTPUT_FOLDER = datetime(2011, 4, 1), datetime(2012, 1, 31), 'complete' # Complete duration
 #START_TIME, END_TIME, WINDOW_OUTPUT_FOLDER = datetime(2011, 5, 1), datetime(2011, 12, 31), 'complete_prop' # Complete propagation duration
 #START_TIME, END_TIME, WINDOW_OUTPUT_FOLDER = datetime(2011, 5, 1), datetime(2011, 8, 31), 'training' # Training duration
-START_TIME, END_TIME, WINDOW_OUTPUT_FOLDER = datetime(2011, 11, 1), datetime(2011, 12, 31), 'testing' # Testing duration
+START_TIME, END_TIME, WINDOW_OUTPUT_FOLDER = datetime(2011, 11, 1), datetime(2011, 11, 30), 'testing' # Testing duration
 
 # Paramters to filter hashtags.
 MIN_HASHTAG_OCCURENCES = 750
