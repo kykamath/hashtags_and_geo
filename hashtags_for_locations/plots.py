35c523e5140ec04827bdfdaf92d0045e5ff5b931
Krishna Kamath <krishna.kamath@gmail.com>
2012-02-27 19:26:32 -0600

    modified models

--- hashtags_for_locations/plots.py
+++ hashtags_for_locations/plots.py
@@ -11,7 +11,8 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 from library.classes import GeneralMethods
 
-for data in iterateJsonFromFile('mr_Data/timeUnitWithOccurrences'):
+#for data in iterateJsonFromFile('mr_Data/timeUnitWithOccurrences'):
+for data in iterateJsonFromFile('mr_Data/1311379200'):
     hashtag_to_points_map = dict([(h, map(lambda lid: getLocationFromLid(lid.replace('_', ' ')), zip(*occs)[1])) for h, occs in groupby(sorted(data['oc'], key=itemgetter(0)), key=itemgetter(0))])
     hashtag_to_grouped_points = [(h, [(point, len(list(occs))) for point, occs in groupby(points, key=itemgetter(0,1))]) for h, points in hashtag_to_points_map.iteritems()]
     hashtagSet = hashtag_to_points_map.keys()
@@ -24,6 +25,7 @@ for data in iterateJsonFromFile('mr_Data/timeUnitWithOccurrences'):
         points, hashtags = zip(*[(location, max(hashtags.iteritems(), key=itemgetter(1))) for location, hashtags in locations_to_hashtag_scores_map.iteritems()])
         print hashtagSet
         hashtag_to_color_map = dict([(hashtag, GeneralMethods.getRandomColor()) for hashtag in hashtagSet])
+        print sorted([(hashtag, len(list(occs))) for hashtag, occs in groupby(sorted(hashtags, key=itemgetter(0)), key=itemgetter(0))], key=itemgetter(1), reverse=True)
         colors = [hashtag_to_color_map[hashtag] for hashtag, score in hashtags]
         plotPointsOnWorldMap(points, blueMarble=False, bkcolor='#CFCFCF', c=colors, lw = 0)
         plt.show()
