5278ccd3b903a1662b98396656a36c8df4b6603c
Krishna Kamath <krishna.kamath@gmail.com>
2012-04-15 01:46:21 -0500

    modified influence

--- locations_influence/settings.py
+++ locations_influence/settings.py
@@ -19,5 +19,5 @@ tuo_location_and_tuo_neighbor_location_and_influence_score_file  = locations_inf
 
 analysis_folder = locations_influence_folder%'analysis/'
 
-hadoop_output_folder = locations_influence_folder%'hadoop'+'/%s/'
+hadoop_output_folder = '/mnt/chevron/kykamath/data/geo/hashtags/locations_influence/hadoop/%s/'
 location_objects_file = hadoop_output_folder+'%s_%s/location_objects'
