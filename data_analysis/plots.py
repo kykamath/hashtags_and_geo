17995d1ac484813242e281d7b99fa9ddeadca0c7
Krishna Kamath <krishna.kamath@gmail.com>
2012-05-23 23:45:45 -0400

    modified plots

--- data_analysis/plots.py
+++ data_analysis/plots.py
@@ -962,6 +962,13 @@ class DataAnalysis():
             if 800<coverage<1600: coverage_boundary=1600
             elif 1600<coverage: coverage_boundary=4000
             return coverage_boundary
+        def print_coverage_stats(ltuo_coverage_and_peak):
+            coverages = zip(*ltuo_coverage_and_peak)[0]
+            total_coverages = len(coverages)+0.
+            coverage_800 = len([coverage for coverage in coverages if coverage==800])
+            coverage_1600 = len([coverage for coverage in coverages if coverage==1600])
+            coverage_4000 = len([coverage for coverage in coverages if coverage==4000])
+            print coverage_800/total_coverages, coverage_1600/total_coverages, coverage_4000/total_coverages
         input_file = f_tuo_hashtag_and_occurrence_count_and_entropy_and_focus_and_coverage_and_peak%(input_files_start_time.strftime('%Y-%m-%d'), input_files_end_time.strftime('%Y-%m-%d'), min_no_of_hashtags)
         ltuo_coverage_and_peak = [(data[4], data[5]) for data in iterateJsonFromFile(input_file)]
         total_hashtags = len(ltuo_coverage_and_peak)+0.
@@ -969,8 +976,11 @@ class DataAnalysis():
         ltuo_coverage_and_peak_gt_6_lt_120 = [(get_coverage_boundary(coverage), peak) for coverage, peak in ltuo_coverage_and_peak if peak>6 and peak<=120]
         ltuo_coverage_and_peak_gt_120 = [(get_coverage_boundary(coverage), peak) for coverage, peak in ltuo_coverage_and_peak if peak>120]
         print 'lt 30: ', len(ltuo_coverage_and_peak_lt_6)/total_hashtags
+        print_coverage_stats(ltuo_coverage_and_peak_lt_6)
         print 'gt 30, lt 600: ', len(ltuo_coverage_and_peak_gt_6_lt_120)/total_hashtags
+        print_coverage_stats(ltuo_coverage_and_peak_gt_6_lt_120)
         print 'gt 600: ', len(ltuo_coverage_and_peak_gt_120)/total_hashtags
+        print_coverage_stats(ltuo_coverage_and_peak_gt_120)
     @staticmethod
     def run():
 #        input_files_start_time, input_files_end_time, min_no_of_hashtags = datetime(2011, 2, 1), datetime(2011, 2, 27), 0
