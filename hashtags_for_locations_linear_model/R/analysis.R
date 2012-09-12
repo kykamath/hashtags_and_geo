18c8bc65d58e186e0f91e0f05405bbf2c55c88d8
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-11 23:54:41 -0500

    modified analysis

--- hashtags_for_locations_linear_model/R/analysis.R
+++ hashtags_for_locations_linear_model/R/analysis.R
@@ -3,7 +3,14 @@
 
 source('../../R/library.R')
 
-data <- getJSONObject(paste('/Users/krishnakamath/Documents/workspace_sept_12/',
-					  		'hashtags_and_geo/R/data.json', sep=''))
-data$menuitem[-length(data$menuitem)]
+getJSONObject <- function(json_file) {
+	library("rjson")
+	print(paste("Reading json from", json_file, sep = " "))
+	return(fromJSON(paste(readLines(json_file), collapse="")))
+}
+
+#data <- getJSONObject('/Users/kykamath/Documents/workspace_sept_2012/hashtags_and_geo/R/data.json')
+data <- getJSONObject('/Users/kykamath/Documents/workspace_sept_2012/hashtags_and_geo/hashtags_for_locations_linear_model/hashtags_with_utm_id_object')
+data
+#data$menuitem[-length(data$menuitem)]
 #getDataFrameFromListOfDicts(data$menuitem)
