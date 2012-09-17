a7d40671b2b79cde3b7bd7ee796a6a5eb7f72b8b
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-16 20:07:40 -0500

    generating data for significant nei

--- hashtags_for_locations_linear_model/analysis.py
+++ hashtags_for_locations_linear_model/analysis.py
@@ -246,7 +246,7 @@ class GeneralAnalysis(object):
             dfm_dict = cjson.decode(df_utm_vectors_json)
             dfm_dict['ltuo_utm_id_to_utm_colnames'] = dict(zip(zip(*ltuo_utm_id_and_vector)[0],
                                                                df_utm_vectors.colnames))
-            dfm_dict.keys()
+            print dfm_dict.keys()
             exit()
             FileIO.writeToFile(cjson.encode(df_utm_vectors_json), output_file)
     @staticmethod
