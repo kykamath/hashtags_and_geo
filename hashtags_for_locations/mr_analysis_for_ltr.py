a4990eea5bc26dfec6158805668060381d74dddd
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-22 02:18:49 -0500

    modified ltr mr analysis

--- hashtags_for_locations/mr_analysis_for_ltr.py
+++ hashtags_for_locations/mr_analysis_for_ltr.py
@@ -126,7 +126,7 @@ class LearningToRank(ModifiedMRJob):
             mf_parameter_names_to_values = dict(self._get_parameter_names_to_values(filtered_train_feature_vectors))
             test_feature_vectors.sort(key=itemgetter('tu'))
             lo_ltuo_hashtag_and_actual_score_and_feature_vector =\
-                                    zip(
+                                    zip(*
                                         [(tu, map(
                                                   itemgetter('hashtag', 'actual_score', 'feature_vector'),
                                                   it_feature_vectors)
@@ -134,24 +134,24 @@ class LearningToRank(ModifiedMRJob):
                                             for tu, it_feature_vectors in 
                                                 groupby(test_feature_vectors, key=itemgetter('tu'))
                                             ]
-                                       )
-#            for ltuo_hashtag_and_actual_score_and_feature_vector in \
-#                    lo_ltuo_hashtag_and_actual_score_and_feature_vector:
-#                yield location, ltuo_hashtag_and_actual_score_and_feature_vector
-                
-                
-                
+                                       )[1]
             for ltuo_hashtag_and_actual_score_and_feature_vector in \
                     lo_ltuo_hashtag_and_actual_score_and_feature_vector:
-                ltuo_hashtag_and_actual_score_and_predicted_score =\
-                            map(lambda (hashtag, actual_score, feature_vector): 
-                                    (
-                                     hashtag,
-                                     actual_score,
-                                     R_Helper.get_predicted_value(mf_parameter_names_to_values, feature_vector)
-                                    ),
-                                ltuo_hashtag_and_actual_score_and_feature_vector)
-                yield location, ltuo_hashtag_and_actual_score_and_predicted_score
+                yield location, ltuo_hashtag_and_actual_score_and_feature_vector
+                
+                
+                
+#            for ltuo_hashtag_and_actual_score_and_feature_vector in \
+#                    lo_ltuo_hashtag_and_actual_score_and_feature_vector:
+#                ltuo_hashtag_and_actual_score_and_predicted_score =\
+#                            map(lambda (hashtag, actual_score, feature_vector): 
+#                                    (
+#                                     hashtag,
+#                                     actual_score,
+#                                     R_Helper.get_predicted_value(mf_parameter_names_to_values, feature_vector)
+#                                    ),
+#                                ltuo_hashtag_and_actual_score_and_feature_vector)
+#                yield location, ltuo_hashtag_and_actual_score_and_predicted_score
             
 #            for ltuo_hashtag_and_actual_score_and_feature_vector in\
 #                     lo_ltuo_hashtag_and_actual_score_and_feature_vector:
