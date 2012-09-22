50e18f576f2ec8821321b038b2d31eda6beecb22
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-22 02:46:45 -0500

    modified files

--- hashtags_for_locations/mr_analysis_for_ltr.py
+++ hashtags_for_locations/mr_analysis_for_ltr.py
@@ -126,7 +126,6 @@ class LearningToRank(ModifiedMRJob):
             mf_parameter_names_to_values = dict(self._get_parameter_names_to_values(filtered_train_feature_vectors))
             test_feature_vectors.sort(key=itemgetter('tu'))
             lo_ltuo_hashtag_and_actual_score_and_feature_vector =\
-                                    zip(
                                         [(tu, map(
                                                   itemgetter('hashtag', 'actual_score', 'feature_vector'),
                                                   it_feature_vectors)
@@ -134,7 +133,7 @@ class LearningToRank(ModifiedMRJob):
                                             for tu, it_feature_vectors in 
                                                 groupby(test_feature_vectors, key=itemgetter('tu'))
                                             ]
-                                       )
+#                                       )[1]
 #            for ltuo_hashtag_and_actual_score_and_feature_vector in \
 #                    lo_ltuo_hashtag_and_actual_score_and_feature_vector:
 #                yield location, ltuo_hashtag_and_actual_score_and_feature_vector
