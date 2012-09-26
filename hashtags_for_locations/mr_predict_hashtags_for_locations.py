47e9a9873c4a50d808d8367528c4ca4bc61535e8
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-26 10:29:56 -0500

    renamed files

--- hashtags_for_locations/mr_predict_hashtags_for_locations.py
+++ hashtags_for_locations/mr_predict_hashtags_for_locations.py
@@ -245,8 +245,8 @@ class OnlineLearning():
                 mf_num_of_hashtags_to_ltuo_tu_and_mf_model_id_to_mf_metric_to_value.iteritems():
 #            accuracy_mf_model_id_to_cumulative_losses = dict([(model_id, 0.0) for model_id in LIST_OF_MODELS])
 #            impact_mf_model_id_to_cumulative_losses = dict([(model_id, 0.0) for model_id in LIST_OF_MODELS])
-            accuracy_mf_model_id_to_cumulative_losses = {}
-            impact_mf_model_id_to_cumulative_losses = {}
+            accuracy_mf_model_id_to_cumulative_losses = defaultdict(list)
+            impact_mf_model_id_to_cumulative_losses = defaultdict(list)
             for tu, mf_model_id_to_mf_metric_to_value in ltuo_tu_and_mf_model_id_to_mf_metric_to_value:
                 accuracy_mf_model_id_to_metric_value = {}
                 impact_mf_model_id_to_metric_value = {}
@@ -269,7 +269,9 @@ class OnlineLearning():
                               ),
                          ]:
                     best_model = get_best_model(mf_model_id_to_cumulative_losses)
-                    mf_num_of_hashtags_to_metric_values[num_of_hashtags] = mf_model_id_to_metric_value[num_of_hashtags]
+                    mf_num_of_hashtags_to_metric_values[num_of_hashtags].append(
+                                                                            mf_model_id_to_metric_value[num_of_hashtags]
+                                                                        )
                     update_losses_for_every_model(
                                                       mf_model_id_to_metric_value,
                                                       best_model,
