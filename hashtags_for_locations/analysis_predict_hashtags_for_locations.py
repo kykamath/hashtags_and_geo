0236fe3f9009c9b1ebdb1a371a92217058200582
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-26 15:24:34 -0500

    renamed files

--- hashtags_for_locations/analysis_predict_hashtags_for_locations.py
+++ hashtags_for_locations/analysis_predict_hashtags_for_locations.py
@@ -6,11 +6,16 @@ Created on Sep 26, 2012
 from mr_predict_hashtags_for_locations import PredictingHastagsForLocations
 from library.mrjobwrapper import runMRJob
 
+dfs_data_folder = 'hdfs:///user/kykamath/geo/hashtags/'
+analysis_folder = '/mnt/chevron/kykamath/data/geo/hashtags/hashtags_for_locations/predict_hashtags_for_locations/%s'
+
+f_prediction_performance = analysis_folder%'prediction_performance'
+
 mr_class = PredictingHastagsForLocations
 output_file = 'output' 
 runMRJob(
          PredictingHastagsForLocations,
-         output_file,
+         f_prediction_performance,
          ['hdfs:///user/kykamath/geo/hashtags/linear_regression'],
          jobconf={'mapred.reduce.tasks':10}
          )
