3e819eaf31d0b00c8338905017d82fbdbf886799
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-26 22:23:15 -0500

    modified mr analysis

--- hashtags_for_locations/mr_predict_hashtags_for_locations.py
+++ hashtags_for_locations/mr_predict_hashtags_for_locations.py
@@ -469,7 +469,7 @@ class PerformanceOfPredictingMethodsByVaryingNumOfHashtags(ModifiedMRJob):
                                'prediction_method': prediction_method,
                                'metric_value': np.mean(list(chain(*lo_metric_values)))
                                }
-        yield performance_summary
+        yield 'o_d', performance_summary
     def steps(self):
 #        return self.predicting_hashtags_for_locations.jobs_to_evaluate_prediction_methods() +\
         return [self.mr(
