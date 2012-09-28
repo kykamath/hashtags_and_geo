c4fcdce51b3aab29d4374d8ea0256b1c39ff3f75
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-28 01:16:05 -0500

    modified mr analysis

--- hashtags_for_locations/analysis_predict_hashtags_for_locations.py
+++ hashtags_for_locations/analysis_predict_hashtags_for_locations.py
@@ -156,7 +156,7 @@ class PredictHashtagsForLocationsPlots():
 #            savefig(output_file_format%metric)
     @staticmethod
     def performance_by_varying_parameter(parameter, input_file):
-        output_file_format = fld_google_drive_data_analysis%GeneralMethods.get_method_id()+'%s_%s.png'
+        output_file_format = fld_google_drive_data_analysis%GeneralMethods.get_method_id()+'/%s_%s.png'
         performance_data = list(FileIO.iterateJsonFromFile(input_file))
         performance_data.sort(key=itemgetter('metric'))
         ltuo_metric_and_ltuo_prediction_method_and_parameter_and_metric_value =\
