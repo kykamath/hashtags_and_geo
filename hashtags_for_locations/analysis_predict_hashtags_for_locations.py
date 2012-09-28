cb980cff3d631ba70381992b7e7b241f71a636e9
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-28 01:22:21 -0500

    modified mr analysis

--- hashtags_for_locations/analysis_predict_hashtags_for_locations.py
+++ hashtags_for_locations/analysis_predict_hashtags_for_locations.py
@@ -205,10 +205,15 @@ class PredictHashtagsForLocationsPlots():
             savefig(output_file_format%(metric))
     @staticmethod
     def run():
+#        PredictHashtagsForLocationsPlots.performance_by_varying_parameter(
+#                                                                  'num_of_hashtags',
+#                                                                  f_performance_of_predicting_by_varying_num_of_hashtags
+#                                                              )
         PredictHashtagsForLocationsPlots.performance_by_varying_parameter(
-                                                                  'num_of_hashtags',
-                                                                  f_performance_of_predicting_by_varying_num_of_hashtags
-                                                              )
+                                                      'prediction_time_interval',
+                                                      f_performance_of_predicting_by_varying_prediction_time_interval
+                                                  )
+        
         
 if __name__ == '__main__':
 #    MRAnalysis.run()
