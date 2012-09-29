992fd4bac0c62e490050eb334e73c3b7cd824309
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-28 21:09:38 -0500

    modified mr analysis

--- hashtags_for_locations/analysis_predict_hashtags_for_locations.py
+++ hashtags_for_locations/analysis_predict_hashtags_for_locations.py
@@ -317,6 +317,7 @@ class PredictHashtagsForLocationsPlots():
     @staticmethod
     def hastag_evolution_in_every_utm_id():
         output_file = fld_google_drive_data_analysis%GeneralMethods.get_method_id()+'.png'
+        plt.figure(num=None, figsize=(4.3, 3))
         mf_x_perct_to_mf_y_perct_to_time_difference = defaultdict(dict)
         for data in FileIO.iterateJsonFromFile(f_propagation_matrix):
             x_perct, y_perct = map(float, data['perct_pair'].split('_'))
@@ -331,25 +332,13 @@ class PredictHashtagsForLocationsPlots():
             y_perct_and_time_difference.sort(key=itemgetter(0), reverse=False)
             y_percts, time_differences = zip(*y_perct_and_time_difference)
             y_percts = map(lambda y: y/100, y_percts)
-            plt.plot(time_differences, y_percts)
-            print y_percts
-            print time_differences
-            print output_file
+            plt.plot(time_differences, y_percts, c='k')
+            plt.scatter(time_differences, y_percts, c='k')
+            plt.grid(True)
+            plt.xlabel('Hashtag propagation time (m)')
+            plt.ylabel('Percentage of hastags occurrences')
             savefig(output_file)
             exit()
-#            Z.append(time_differences)
-##        ax = plt.subplot(111)
-#        im = plt.imshow(Z, cmap=cm.binary)
-##        plt.xlim(xmin=0, xmax=50)
-##        plt.ylim(ymin=0, ymax=50)
-##        yticks = plt.yticks()
-##        plt.yticks([0, 1])
-##        plt.yticks()
-##        im.set_interpolation('nearest')
-##        im.set_interpolation('bicubic')
-#        im.set_interpolation('bilinear')
-#        plt.colorbar(im)
-#        plt.show()
     @staticmethod
     def run():
 #        PredictHashtagsForLocationsPlots.performance_by_varying_num_of_hashtags()
