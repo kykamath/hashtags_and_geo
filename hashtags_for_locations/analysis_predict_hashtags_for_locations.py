bb8135bba246bbc078121f8740c4d0330d851702
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-29 20:27:16 -0500

    modified mr analysis

--- hashtags_for_locations/analysis_predict_hashtags_for_locations.py
+++ hashtags_for_locations/analysis_predict_hashtags_for_locations.py
@@ -420,12 +420,10 @@ class PredictHashtagsForLocationsPlots():
         ltuo_majority_threshold_bucket_time_and_num_of_utm_ids =\
                                                          mf_majority_threshold_bucket_time_to_num_of_utm_ids.items()
         ltuo_majority_threshold_bucket_time_and_num_of_utm_ids.sort(key=itemgetter(0))
-        total_num_of_utm_ids = sum(
-                                   ltuo_majority_threshold_bucket_time_and_num_of_utm_ids,
-                                   key=itemgetter(1)
-                                )
         majority_threshold_bucket_time, num_of_utm_ids = zip(*ltuo_majority_threshold_bucket_time_and_num_of_utm_ids)
+        total_num_of_utm_ids = sum(num_of_utm_ids)
         perct_of_utm_ids = [n/total_num_of_utm_ids for n in num_of_utm_ids]
+        print sum(perct_of_utm_ids)
         plt.plot(majority_threshold_bucket_time, perct_of_utm_ids)
         savefig(output_file)
 #        for majority_threshold_bucket_time, num_of_utm_ids in\
