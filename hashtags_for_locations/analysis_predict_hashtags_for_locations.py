540ff44e3a3b605fac96bc6b4863649af2f81c4f
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-30 15:29:30 -0500

    modified mr analysis

--- hashtags_for_locations/analysis_predict_hashtags_for_locations.py
+++ hashtags_for_locations/analysis_predict_hashtags_for_locations.py
@@ -470,15 +470,17 @@ class PredictHashtagsForLocationsPlots():
                 ltuo_min_common_hashtag_and_mean_propagation_statuses:
             print min_common_hashtag
             density = gaussian_kde(mean_propagation_statuses)
-            xs = np.linspace(-1,1,21)
+            xs = np.linspace(-1,1,100)
             density.covariance_factor = lambda : .25
             density._compute_covariance()
             ys = density(xs)
             total_ys = sum(ys)
             ys = [y/total_ys for y in ys]
             plt.plot(xs, ys, c='y')
+            plt.xlabel('Impact of using a location to predict hashtags in another')
+            plt.ylabel('% of locations')
             savefig(output_file_format%min_common_hashtag)
-            break
+#            break
 #    @staticmethod
 #    def example_of_hashtag_propagation_patterns():
 #        output_file_format = fld_google_drive_data_analysis%GeneralMethods.get_method_id()+'/%s.png'
