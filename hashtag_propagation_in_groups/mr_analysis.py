f3be3545176fd628e4ae3a3cf5c0d100b1862e27
Krishna Kamath <krishna.kamath@gmail.com>
2012-10-05 11:28:06 -0500

    modified files

--- hashtag_propagation_in_groups/mr_analysis.py
+++ hashtag_propagation_in_groups/mr_analysis.py
@@ -128,9 +128,10 @@ class WordObjectExtractor(ModifiedMRJob):
         for (occ_time, occ_location),(_, words) in zip(ltuo_occ_time_and_occ_location, ltuo_occ_time_and_words):
             for word in words:
                 if hashtag not in self.mf_word_to_mf_hashtag_to_ltuo_occ_time_and_occ_location[word]:
-                    self.mf_word_to_mf_hashtag_to_ltuo_occ_time_and_occ_location[word][hashtag].append(
-                                                                                               [occ_time, occ_location]
-                                                                                           )
+                    self.mf_word_to_mf_hashtag_to_ltuo_occ_time_and_occ_location[word][hashtag] = []
+                self.mf_word_to_mf_hashtag_to_ltuo_occ_time_and_occ_location[word][hashtag].append(
+                                                                                           [occ_time, occ_location]
+                                                                                       )
     def mapper_final(self):
         for word, mf_hashtag_to_ltuo_occ_time_and_occ_location in\
                 self.mf_word_to_mf_hashtag_to_ltuo_occ_time_and_occ_location.iteritems():
