a3602a4c1404c132a3087ebddd2b9be1c792262e
Krishna Kamath <krishna.kamath@gmail.com>
2012-10-06 00:46:21 -0500

    modified files

--- hashtag_propagation_in_groups/analysis.py
+++ hashtag_propagation_in_groups/analysis.py
@@ -9,8 +9,11 @@ from library.file_io import FileIO
 from library.mrjobwrapper import runMRJob
 from mr_analysis import HashtagsExtractor
 from mr_analysis import PARAMS_DICT
+from mr_analysis import WordObjectExtractor
 from pprint import pprint
 from settings import f_hashtags_extractor
+from settings import f_hdfs_hashtags
+from settings import f_word_objects_extractor
 from settings import hdfs_input_folder
 import time
 
@@ -20,10 +23,12 @@ class MRAnalysis():
         PARAMS_DICT['input_files_start_time'] = time.mktime(input_files_start_time.timetuple())
         PARAMS_DICT['input_files_end_time'] = time.mktime(input_files_end_time.timetuple())
         print 'Running map reduce with the following params:', pprint(PARAMS_DICT)
-        runMRJob(mr_class,
-                 output_file,
-                 MRAnalysis.get_input_files_with_tweets(input_files_start_time, input_files_end_time),
-                 jobconf={'mapred.reduce.tasks':500})
+        runMRJob(
+                     mr_class,
+                     output_file,
+                     MRAnalysis.get_input_files_with_tweets(input_files_start_time, input_files_end_time),
+                     jobconf={'mapred.reduce.tasks':500}
+                 )
         FileIO.writeToFileAsJson(PARAMS_DICT, output_file)
     @staticmethod
     def get_input_files_with_tweets(startTime, endTime, folderType='world'):
@@ -39,10 +44,21 @@ class MRAnalysis():
         output_file = f_hashtags_extractor
         MRAnalysis.run_job(mr_class, output_file, input_files_start_time, input_files_end_time)
     @staticmethod
+    def word_object_extractor():
+        mr_class = WordObjectExtractor
+        output_file = f_word_objects_extractor
+        runMRJob(
+                     mr_class,
+                     output_file,
+                     [f_hdfs_hashtags],
+                     jobconf={'mapred.reduce.tasks':500}
+                 )
+    @staticmethod
     def run():
         input_files_start_time, input_files_end_time = \
                                 datetime(2011, 2, 1), datetime(2012, 8, 31)
-        MRAnalysis.hashtags_extractor(input_files_start_time, input_files_end_time)
+#        MRAnalysis.hashtags_extractor(input_files_start_time, input_files_end_time)
+        MRAnalysis.word_object_extractor()
 
 if __name__ == '__main__':
     MRAnalysis.run()
--- hashtag_propagation_in_groups/settings.py
+++ hashtag_propagation_in_groups/settings.py
@@ -7,5 +7,8 @@ hdfs_input_folder = 'hdfs:///user/kykamath/geo/hashtags/%s/'
 
 fld_data_analysis = '/mnt/chevron/kykamath/data/geo/hashtags/hashtag_propagation_in_groups/%s/'
 
+f_hdfs_hashtags = hdfs_input_folder%'2011_2_to_2012_8'+'/min_num_of_hashtags_50_with_words/hashtags'
+
 f_hashtags_extractor = fld_data_analysis%'hashtags_extractor'+'hashtags'
+f_word_objects_extractor = fld_data_analysis%'word_objects_extractor'+ 'word_objects'
     
