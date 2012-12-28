24b6357c3ffef878afff7a3aba86e05ab6fadf3f
Krishna Kamath <krishna.kamath@gmail.com>
2012-12-28 15:54:01 -0600

    moified files

--- hashtags_for_locations/analysis_predict_hashtags_for_locations.py
+++ hashtags_for_locations/analysis_predict_hashtags_for_locations.py
@@ -1223,7 +1223,7 @@ class PerformanceByLocationAnalysis(object):
                     if isWithinBoundingBox(location, boundary):
                         mf_us_boundary_to_scores[id].append(score)
                         break
-            print len(scores), len(locations), mf_us_boundary_to_scores.items()
+            print len(scores), len(locations), [(k, len(v)) for k, v in mf_us_boundary_to_scores.iteritems()]
         raw_data = list(FileIO.iterateJsonFromFile(f_performance_by_location, True))
         getLocation = lambda lid: getLocationFromLid(lid.replace('_', ' '))
         locations = map(getLocation, map(itemgetter('location'), raw_data))
