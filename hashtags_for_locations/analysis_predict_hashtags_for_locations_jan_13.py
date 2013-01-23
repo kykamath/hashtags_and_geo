5a7b6527ab91b4e81de2ff49eaa6b049472bfa20
Krishna Kamath <krishna.kamath@gmail.com>
2013-01-23 09:04:41 -0600

    modified files

--- hashtags_for_locations/analysis_predict_hashtags_for_locations_jan_13.py
+++ hashtags_for_locations/analysis_predict_hashtags_for_locations_jan_13.py
@@ -11,24 +11,20 @@ from datetime import timedelta
 from library.mrjobwrapper import runMRJob
 from mr_predict_hashtags_for_locations_jan_13 import HashtagsByModelsByLocations
 
-TIME_UNIT_IN_SECONDS = 60*60
+TIME_UNIT_IN_SECONDS = 60 * 60
 
 dfs_data_folder = 'hdfs:///user/kykamath/geo/hashtags/2011-09-01_2011-11-01/'
 dfs_input = dfs_data_folder + '360_120/100/linear_regression'
 
 analysis_folder = '/mnt/chevron/kykamath/data/geo/hashtags/hashtags_for_locations/predict_hashtags_for_locations/%s'
-f_hashtags_by_models_by_locations = analysis_folder%'hashtags_by_models_by_locations'
+f_hashtags_by_models_by_locations = analysis_folder % 'hashtags_by_models_by_locations'
 
 class MRAnalysis(object):
     @staticmethod
     def get_input_files():
         for j in range(1, 7):
-            predictionTimeInterval = timedelta(seconds=j*TIME_UNIT_IN_SECONDS)
-            yield dfs_data_folder+'/%s_%s/100/linear_regression'%(
-                                                                  dfs_data_folder,
-                                                                  360,
-                                                                  predictionTimeInterval.seconds/60
-                                                                )
+            predictionTimeInterval = timedelta(seconds=j * TIME_UNIT_IN_SECONDS)
+            yield '%s/%s_%s/100/linear_regression' % (dfs_data_folder, 360, predictionTimeInterval.seconds / 60)
     @staticmethod
     def hashtags_by_models_by_locations():
         runMRJob(
