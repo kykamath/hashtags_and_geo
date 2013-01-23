d3196b02888692c9907173481cdbb656d26c7a68
Krishna Kamath <krishna.kamath@gmail.com>
2013-01-23 08:57:36 -0600

    modified files

--- hashtags_for_locations/analysis_predict_hashtags_for_locations_jan_13.py
+++ hashtags_for_locations/analysis_predict_hashtags_for_locations_jan_13.py
@@ -6,22 +6,37 @@ Created on Jan 22, 2013
 #import sys
 #sys.path.append('../')
 
+from datetime import datetime
+from datetime import timedelta
 from library.mrjobwrapper import runMRJob
 from mr_predict_hashtags_for_locations_jan_13 import HashtagsByModelsByLocations
 
-dfs_input = 'hdfs:///user/kykamath/geo/hashtags/2011-09-01_2011-11-01/360_120/100/linear_regression'
+TIME_UNIT_IN_SECONDS = 60*60
+
+dfs_data_folder = 'hdfs:///user/kykamath/geo/hashtags/2011-09-01_2011-11-01/'
+dfs_input = dfs_data_folder + '360_120/100/linear_regression'
 
 analysis_folder = '/mnt/chevron/kykamath/data/geo/hashtags/hashtags_for_locations/predict_hashtags_for_locations/%s'
 f_hashtags_by_models_by_locations = analysis_folder%'hashtags_by_models_by_locations'
 
 class MRAnalysis(object):
     @staticmethod
+    def get_input_files():
+        for j in range(1, 7):
+            predictionTimeInterval = timedelta(seconds=j*TIME_UNIT_IN_SECONDS)
+            yield '%s2011-09-01_2011-11-01/%s_%s/100/linear_regression'%(
+                                                                          dfs_data_folder,
+                                                                          360,
+                                                                          predictionTimeInterval.seconds/60
+                                                                        )
+    @staticmethod
     def hashtags_by_models_by_locations():
         runMRJob(
-                     HashtagsByModelsByLocations,
-                     f_hashtags_by_models_by_locations,
-                     [dfs_input],
-                     jobconf={'mapred.reduce.tasks':500, 'mapred.task.timeout': 86400000}
+                    HashtagsByModelsByLocations,
+                    f_hashtags_by_models_by_locations,
+#                     [dfs_input],
+                    MRAnalysis.get_input_files(),
+                    jobconf={'mapred.reduce.tasks':500, 'mapred.task.timeout': 86400000}
                  )
     @staticmethod
     def run():
@@ -29,3 +44,4 @@ class MRAnalysis(object):
 
 if __name__ == '__main__':
     MRAnalysis.run()
+    
